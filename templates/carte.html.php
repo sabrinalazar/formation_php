c7a2ce06b0e7143a8c40bf96ceea05075f31505c
sabrina lazar <sabrina.lazar@hotmail.fr>
2019-01-16 14:55:11 +0100

    premier commit

---
+++ templates/carte.html.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <title>carte </title>
+    <body>
+
+        <style>h1{ color : greenyellow}
+            .gmaps-frame {
+                width: 910px;
+                height: 600px;
+                border: 0;
+                margin: auto;
+                display: block;
+            }</style>
+        <iframe class="gmaps-frame" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d494128.1250830646!2d-61.2939732545665!3d14.634586546664524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c6aa0f90066070d%3A0xe1001b1217afe7b0!2sMartinique!5e0!3m2!1sfr!2sfr!4v1547645367984" width="600" height="450" frameborder="0" style="border:0" allow</body>fullscreen></iframe>
+
+</body>
