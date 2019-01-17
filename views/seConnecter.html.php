4abf51a0edb46980e1c69636923f4be7d01d9da7
Karl <cleonkarl.1980@gmail.com>
2019-01-17 13:18:08 +0100

    creation page se connecter

---
+++ views/seConnecter.html.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="../style/seConnecter.css"/>
+	<title>connexion Blogtrotteuze</title>
+</head>
+<body>
+	<form action="connexion.php" method='post'>
+		<div id="conteneur">
+
+			<strong>Identifiant</strong>
+			<input type="text" name="login"  maxlength="250">
+
+
+			<strong>Mot de Passe</strong>
+			<input type="password" name="mdp"  maxlength="20">
+
+			<strong>Se connecter</strong>
+			<input type="submit" name="Se connecter" value="Valider">
+
+		</div>
+
+	</form>
+</body>
+</html>
