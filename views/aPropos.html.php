d9ec4a13603c81974dc09c263acb39ad6d07f84d
Karl <cleonkarl.1980@gmail.com>
2019-01-17 13:15:35 +0100

    creation page a propos

---
+++ views/aPropos.html.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<p>Créee en Janvier 2019, notre blalala blalala a pour but de blala blala...
+	h.s.k</p>
+</body>
+</html>
