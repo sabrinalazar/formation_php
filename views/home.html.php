cf9ab7c31038dd7958bc944ebab7ad6c682829fd
B.hala <boudjaatat.hala@outlook.fr>
2019-01-17 14:19:53 +0100

    modiiif

--- views/home.html.php
+++ views/home.html.php
@@ -41,9 +41,10 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto" focusable="false" role="img"><title>Product</title><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
                 </a>
 
-                <a class="py-2 d-none d-md-inline-block" href="#">A propos de nous </a>
-                <a class="py-2 d-none d-md-inline-block" href="#">Acceuil</a>
-                <a class="py-2 d-none d-md-inline-block" href="#">Se connecter</a>
+                <a class="py-2 d-none d-md-inline-block" href="home.html.php">Accueil</a>
+                <a class="py-2 d-none d-md-inline-block" href="inscription.html.php">Inscription </a>
+                <a class="py-2 d-none d-md-inline-block" href="aPropos.html.php">A propos de nous </a>
+                <a class="py-2 d-none d-md-inline-block" href="seConnecter.html.php">Se connecter</a>
 
             </div>
         </nav>
@@ -55,7 +56,7 @@
             <div class="col-md-5 p-lg-5 mx-auto my-5">
                 <h1 class="display-4 font-weight-normal">Realise ton rêve! </h1>
                 <p class="lead font-weight-normal">vous avez de l'argent ? ou PAS ?! Notre agence va vous aider à choisir LE voyage qui vous corresponds le mieux :) </p>
-                <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+                <a class="btn btn-outline-secondary" href="#">let's go</a>
             </div>
             <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
         </div>
@@ -73,7 +74,7 @@
                                         <div class="d-flex justify-content-between align-items-center">
                                             <div class="btn-group">
                                                 <button type="button" class="btn btn-sm btn-outline-secondary">Visiter</button>
-                                                <button type="button" class="btn btn-sm btn-outline-secondary">Acheter</button>
+                                                <button type="button" class="btn btn-sm btn-outline-secondary">Acheter votre billet</button>
                                             </div>
                                             <small class="text-muted">9 mins</small>
                                         </div>
@@ -88,7 +89,7 @@
                                         <div class="d-flex justify-content-between align-items-center">
                                             <div class="btn-group">
                                                 <button type="button" class="btn btn-sm btn-outline-secondary">Visiter</button>
-                                                <button type="button" class="btn btn-sm btn-outline-secondary">Acheter</button>
+                                                <button type="button" class="btn btn-sm btn-outline-secondary">Acheter votre billet</button>
                                             </div>
                                             <small class="text-muted">9 mins</small>
                                         </div>
@@ -105,7 +106,7 @@
                                         <div class="d-flex justify-content-between align-items-center">
                                             <div class="btn-group">
                                                 <button type="button" class="btn btn-sm btn-outline-secondary">Visiter</button>
-                                                <button type="button" class="btn btn-sm btn-outline-secondary">Acheter</button>
+                                                <button type="button" class="btn btn-sm btn-outline-secondary">Acheter votre billet</button>
                                             </div>
                                             <small class="text-muted">9 mins</small>
                                         </div>
