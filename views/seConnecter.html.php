0ee678cefc7ce2dcd84344314bf5d84c7b0d9ab0
Karl <cleonkarl.1980@gmail.com>
2019-01-17 13:35:41 +0100

    ca ma gonfler

--- views/seConnecter.html.php
+++ views/seConnecter.html.php
@@ -1,26 +1,43 @@
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" href="../style/seConnecter.css"/>
-	<title>connexion Blogtrotteuze</title>
-</head>
-<body>
-	<form action="connexion.php" method='post'>
-		<div id="conteneur">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+        <link href="../style/seConnecter.css" rel="stylesheet">
 
-			<strong>Identifiant</strong>
-			<input type="text" name="login"  maxlength="250">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Connexion</title>
 
 
-			<strong>Mot de Passe</strong>
-			<input type="password" name="mdp"  maxlength="20">
+    <!-- Custom styles for this template -->
+  </head>
 
-			<strong>Se connecter</strong>
-			<input type="submit" name="Se connecter" value="Valider">
+  <body class="text-center">
+    <form class="form-signin">
+      <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+    </form>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
 
-		</div>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
 
-	</form>
-</body>
+  </body>
 </html>
