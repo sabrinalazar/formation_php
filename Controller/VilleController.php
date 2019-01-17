b0c249d8aef9e543de64a002e57e5fa225575043
B.hala <boudjaatat.hala@outlook.fr>
2019-01-17 11:55:03 +0100

    modif

--- Controller/VilleController.php
+++ Controller/VilleController.php
@@ -4,27 +4,25 @@ class VilleController {
 
 //afficher le détail d'une ville
 
-    public function detail($id) {
+    public function detail($id)
+    {
         $vm = new VilleManager();
         $ville = $vm->getLaville($id);
         var_dump($ville);
         $this->render('detail.html.php', ['ville' => $ville, 'titre' => 'Détail de la ville']);
 
 
-        //
-        extract($data);
-
-        //demarrer la mise en tampon
-        ob_start();
-
-        //inclure le bout de vue recupere
-        include __DIR__ . './../views/' . vue;
-
-        //le tampon sous forme de caractère
-        $buffer = ob_get_clean();
-        //nettoyer le tampon
-
-        include __DIR__ . '/./../views/home.html.php';
     }
+
+        private function render($vue,$data=[]){
+            extract($data);
+
+            ob_start();
+            include __DIR__.'/../views/'.$vue;
+            $buffer = ob_get_clean();
+            include __DIR__.'/../views/home.html.php';
+
+
+        }
 
 }
